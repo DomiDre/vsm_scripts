c67494b01943c746bd7eb283b3d25ea17ca5099a
Dominique Dresen <dominique.dresen@uni-koeln.de>
2016-10-04 22:27:04 +0200

    Superclass was added for all vsm classes. Containing version number and standard call routine.

---
+++ vsm.py
@@ -0,0 +1,27 @@
+import sys
+
+class VSMClass():
+    def __init__(self):
+        self.version = 1.1
+        self.n_args = len(sys.argv) - 1
+        self.get_args()
+        if "-help" in sys.argv:
+            self.help()
+            
+    def get_args(self):
+        self.arg_dict = {}
+        for ip, param in enumerate(sys.argv):
+            if param.startswith("-"):
+                jp = ip + 1
+                param_list = []
+                while jp <= self.n_args and not sys.argv[jp].startswith("-"):
+                    param_list.append(sys.argv[jp])
+                    jp += 1
+                if len(param_list) == 1:
+                    param_list = param_list[0]
+                self.arg_dict[param[1:]] = param_list
+            else:
+                continue
+                
+    def help(self):
+        print("Help is not defined.")
